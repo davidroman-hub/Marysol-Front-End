23b08cc6bafda859852531c4dc65758bf6c97e88
davidroman-hub <jobroman83@gmail.com>
2020-03-24 01:12:10 +0100

    adding some styles and translating

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,36 +1,48 @@
 import React,{Fragment} from 'react';
 import {Link, withRouter} from 'react-router-dom'
 import {isAuth, signout} from '../auth/helpers'
+import Logo from './logo12.png'
+
 
 
 const Layout = ({children,match,history}) => {
 
+
+
 const isActive = path => {
     if(match.path === path) {
-        return {color:'#000'}
+        return {color:'#ff9900'}
     } else{
-        return {color:'#fff'}
+        return {color:'#000'}
     }
 };
 
 
     const nav = () => (
-        <ul className="nav nav-tabs bg-primary">
-                <li className="nav-item">
-                    <Link to='/' className=" nav-link" style={isActive('/')}>
-                         {/* home / {JSON.stringify(history)}     */}
-                         home
-                    </Link>
-                </li>
+        <ul className="navbar navbar-expand-lg navbar-dark bg-dark text-center">
+                <Link className='nav-color' class="nav-link" to='/'>
+                    <img alt='logo' width='80px' height='80px' src={Logo}/>
+                </Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                    <li className="nav-item">
+                        <Link to='/' className=" nav-link" style={isActive('/')}>
+                            {/* home / {JSON.stringify(history)}     */}
+                            Nosotros
+                        </Link>
+                    </li>
 
                 {!isAuth () && (
                     <Fragment>
                         <li className="nav-item">
-                            <Link to='/signup' className=" nav-link" style={isActive('/signup')}> SignUp </Link>
+                            <Link to='/signup' className=" nav-link" style={isActive('/signup')}> Registrate </Link>
                         </li>
                         
                         <li className="nav-item">
-                            <Link to='/signin' className=" nav-link" style={isActive('/signin')}> SignIn </Link>
+                            <Link to='/signin' className=" nav-link" style={isActive('/signin')}> Inicia Sesión </Link>
                         </li>
                 </Fragment>
                 )}
@@ -66,6 +78,8 @@ const isActive = path => {
                         </li>                 
                       
                 )}
+                    </div>
+                </div>
         </ul>
     )
     return(
