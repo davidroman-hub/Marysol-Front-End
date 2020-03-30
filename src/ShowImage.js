5dfe99be909e7e5da226a9c2f7af05301e22b923
davidroman-hub <jobroman83@gmail.com>
2020-03-30 17:29:38 +0100

    adding images to the cards

---
+++ src/ShowImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {API} from './Config'
+
+const ShowImage = ({item,url}) => (
+    <div className='product-img'>
+        <img src={`${API}/${url}/photo/${item._id}`}
+        alt={item.name}
+        style={{maxHeight:'100%', maxWidth:'100%'}}/>
+    </div>
+
+)
+
+export default ShowImage
--- src/admin/Card.js
+++ src/admin/Card.js
@@ -2,36 +2,15 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import photo from './img_avatar.png'
 import './Card.scss'
-// const Card = ({product}) => {
-
-//     return (
-//         <div className='col-4 mb-3'>
-//         <div className='card'>
-//             <div className='card-header'>{product.name}</div>
-//             <div className='card-body'>
-//                 <p>{product.description}</p>
-//                 <p>{product.price}</p>
-//                 <Link to='/'/>
-//                 <button className='btn btn-outline-primary mt-2 mb-2'>
-//                     View Product
-//                 </button>
-//                 <button className='btn btn-outline-warning mt-2 mb-2'>
-//                     Add to card
-//                 </button>
-//             </div>
-//         </div>
-//     </div>
-//     )
-// }
-
-// export default Card
+import ShowImage from '../ShowImage'
 
 
 const Card = ({product}) => {
 
     return (
         <div className="card1">
-            <img src={photo} alt="avatar" style={{width:"100%"}} />
+            {/* <img src={photo} alt="avatar" style={{width:"100%"}} /> */}
+            <ShowImage item={product} url='product' />
        
             <div className="card-container">
                 <h4 className="text-center">{product.name}</h4>
